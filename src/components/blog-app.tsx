abeed6f08611889bb7761a73883aad16a249bf80
Dharshansk16 <dcodecraft@gmail.com>
2025-07-24 00:51:15 +0530

    transition refactor

--- src/components/blog-app.tsx
+++ src/components/blog-app.tsx
@@ -62,9 +62,9 @@ export default function BlogApp({ onBack }: BlogAppProps) {
 
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
+      initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6, ease: "linear" }}
+      transition={{ duration: 0.2, ease: "linear" }}
       className="min-h-screen p-8"
     >
       {/* Header */}
